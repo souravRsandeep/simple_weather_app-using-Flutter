eccc2d8f710c9aad894327f55eae29c2985716f7
Veeresh B V <veereshbv04@gmail.com>
2020-11-09 17:20:59 +0530

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Veeresh B V\Documents\Flutter_Mobile\Flutter Softwares\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Veeresh B V\Documents\Flutter_Mobile\Projects\weather_app\Flutter-Simple-Weather"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Veeresh B V\Documents\Flutter_Mobile\Flutter Softwares\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
